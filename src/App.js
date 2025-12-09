d9d47578a32acb766f06ab5ebc31cb9bc5f451ef
CunMayday <caltinoz@purdueglobal.edu>
2025-12-08 21:59:48 -0500

    Add Clerk authentication gating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-   import './App.css';
-   import StudentScoreAnalyzer from './components/StudentScoreAnalyzer';
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
+import './App.css';
+import StudentScoreAnalyzer from './components/StudentScoreAnalyzer';
 
-   function App() {
-     return (
-       <div className="App">
-         <StudentScoreAnalyzer />
-       </div>
-     );
-   }
+function App() {
+  return (
+    <div className="App">
+      <SignedIn>
+        <StudentScoreAnalyzer />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </div>
+  );
+}
 
-   export default App;
+export default App;
